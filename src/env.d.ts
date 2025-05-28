9148c3efaa87b7b2f69f844245cb583070bb1bfd
tiagosantiagomkt <tiagosantiagomkt@gmail.com>
2025-05-28 13:06:03 -0300

    Add Stripe public key to environment configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,9 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_STRIPE_PUBLIC_KEY: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
-} 
+}
